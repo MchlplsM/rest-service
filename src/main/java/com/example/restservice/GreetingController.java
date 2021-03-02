eb1ebbc240690865a8205b456392248b945b9b01
Marios Michalopoulos <michalopoulos.m14@gmail.com>
2021-03-02 08:38:07 +0200

    Added some comments

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+                //It is the Controller Layer/AP Layer (Will receive and handle HTTP requests like GET,POST etc)
 @RestController //It is going to be a class that will define REQUEST MAPPING
 public class GreetingController {
 
+    //With -final- keyword you are not able to change the value.
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong(); //A way to set the ID for that greeting
 
